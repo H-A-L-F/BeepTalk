5f195fa607087ae34a4313a6c0066d435741d70c
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-11-10 17:13:39 +0700

    upvote downvote number

---
+++ app/src/main/java/com/example/beeptalk/ThreadPage.kt
@@ -0,0 +1,11 @@
+package com.example.beeptalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThreadPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thread_page)
+    }
+}
