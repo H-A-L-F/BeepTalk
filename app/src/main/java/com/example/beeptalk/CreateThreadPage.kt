ae00ba4ed2750730971ada5c8bff151413746ec9
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-11-14 18:24:12 +0700

    post thread

--- app/src/main/java/com/example/beeptalk/CreateThreadPage.kt
+++ app/src/main/java/com/example/beeptalk/CreateThreadPage.kt
@@ -2,10 +2,38 @@ package com.example.beeptalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.beeptalk.databinding.ActivityCreateThreadPageBinding
+import com.example.beeptalk.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CreateThreadPage : AppCompatActivity() {
+    private lateinit var binding : ActivityCreateThreadPageBinding
+    private lateinit var db : FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_thread_page)
+        binding = ActivityCreateThreadPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPost.setOnClickListener {
+            val threadBody = binding.etThreadBody.text.toString()
+
+            val Thread = Thread(threadBody)
+
+            db = FirebaseFirestore.getInstance()
+            db.collection("threads").add(Thread).addOnSuccessListener {
+                binding.etThreadBody.text.clear()
+
+                Toast.makeText(this, "Posted thread", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Post failed", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/beeptalk/Thread.kt
@@ -0,0 +1,5 @@
+package com.example.beeptalk
+
+data class Thread(val body : String, val stitch : String? = null) {
+
+}
