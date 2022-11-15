9a2fb1ddf6b12b56575d8149c774db10485e999a
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-11-15 11:21:25 +0700

    thread rv adapter

---
+++ app/src/main/java/com/example/beeptalk/lib/ThreadRVAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.beeptalk.lib
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.beeptalk.databinding.ActivityThreadPageBinding
+import com.example.beeptalk.databinding.CardThreadBinding
+import java.util.*
+import com.example.beeptalk.models.Thread
+
+class ThreadRVAdapter(private val threads : Vector<Thread>): RecyclerView.Adapter<ThreadRVAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: CardThreadBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(CardThreadBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val thread : Thread = threads[position]
+        holder.binding.tvCreatedAt.text = thread.createdAt.toString()
+        holder.binding.tvThreadBody.text = thread.body
+        holder.binding.tvTotalVotes.text = thread.getTotalVotes().toString()
+
+        holder.binding.cardThread.setOnClickListener {
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return threads.size
+    }
+}
--- app/src/main/java/com/example/beeptalk/models/Thread.kt
+++ app/src/main/java/com/example/beeptalk/models/Thread.kt
@@ -4,5 +4,8 @@ import com.google.firebase.Timestamp
 import java.util.*
 
 data class Thread(val body : String, val stitch : String? = null, val upvote : Int = 0, val downvote : Int = 0, val createdAt : Date = Timestamp.now().toDate()) {
-    
+
+    public fun getTotalVotes(): Int {
+        return upvote - downvote
+    }
 }
