130422df752bd988a3d53295fba225f711ef6729
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-11-15 16:41:57 +0700

    cleanup + hardcode margintop on card

--- app/src/main/java/com/example/beeptalk/lib/ThreadRVAdapter.kt
+++ app/src/main/java/com/example/beeptalk/lib/ThreadRVAdapter.kt
@@ -26,18 +26,14 @@ class ThreadRVAdapter(private val threads : ArrayList<Thread>): RecyclerView.Ada
         holder.binding.tvTotalVotes.text = thread.getTotalVotes().toString()
 
         holder.binding.btnUpvote.setOnClickListener {
-            Log.d("votes", "onBindViewHolder: ${thread.upvote}")
             thread.upvote++
-            Log.d("votes", "onBindViewHolder: ${thread.upvote}")
             holder.binding.tvTotalVotes.text = thread.getTotalVotes().toString()
             var db = FirebaseFirestore.getInstance()
             db.collection("threads").document(thread.id).update("upvote", FieldValue.increment(1))
         }
 
         holder.binding.btnDownvote.setOnClickListener {
-            Log.d("votes", "onBindViewHolder: ${thread.downvote}")
             thread.downvote++
-            Log.d("votes", "onBindViewHolder: ${thread.downvote}")
             holder.binding.tvTotalVotes.text = thread.getTotalVotes().toString()
             var db = FirebaseFirestore.getInstance()
             db.collection("threads").document(thread.id).update("downvote", FieldValue.increment(1))
