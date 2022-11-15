7517e6587bfed36a22433becaa7f15ef460b375e
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-11-15 21:02:05 +0700

    thread detail page + comment card

---
+++ app/src/main/java/com/example/beeptalk/pages/ThreadDetailPage.kt
@@ -0,0 +1,11 @@
+package com.example.beeptalk.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThreadDetailPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thread_detail_page)
+    }
+}
