a3821c0e52180192de1a8eeab5f338b1ed875a97
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-11-16 14:07:35 +0700

    working rv comment in threadpage

--- app/src/main/java/com/example/beeptalk/pages/ThreadDetailPage.kt
+++ app/src/main/java/com/example/beeptalk/pages/ThreadDetailPage.kt
@@ -3,15 +3,21 @@ package com.example.beeptalk.pages
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.beeptalk.databinding.ActivityThreadDetailPageBinding
+import com.example.beeptalk.lib.RecyclerViewInterface
+import com.example.beeptalk.lib.ThreadCommentRVAdapter
+import com.example.beeptalk.lib.ThreadRVAdapter
 import com.example.beeptalk.models.Thread
 import com.example.beeptalk.models.ThreadComment
 import com.example.beeptalk.parcel.ThreadID
 import com.google.firebase.firestore.FirebaseFirestore
 
-class ThreadDetailPage : AppCompatActivity() {
+class ThreadDetailPage : AppCompatActivity(), RecyclerViewInterface {
 
     private lateinit var binding: ActivityThreadDetailPageBinding
+    private lateinit var comments : ArrayList<ThreadComment>
+    private lateinit var threadCommentRVAdapter: ThreadCommentRVAdapter
     private lateinit var db : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,5 +48,40 @@ class ThreadDetailPage : AppCompatActivity() {
                     Toast.makeText(this, "Comment failed", Toast.LENGTH_SHORT).show()
                 }
         }
+
+        binding.rvThreadComment.layoutManager = LinearLayoutManager(this)
+        binding.rvThreadComment.setHasFixedSize(true)
+
+        comments = arrayListOf()
+
+        threadCommentRVAdapter = ThreadCommentRVAdapter(comments, this)
+
+        binding.rvThreadComment.adapter = threadCommentRVAdapter
+
+        subscribeThreadComments(thread.id)
+    }
+
+    private fun subscribeThreadComments(threadId : String) {
+        db.collection("threads").document(threadId).collection("comments")
+            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                firebaseFirestoreException?.let {
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    return@addSnapshotListener
+                }
+
+                querySnapshot?.let {
+                    for (document in querySnapshot.documents) {
+                        var curr = document.toObject(ThreadComment::class.java)
+                        curr?.id = document.id.toString()
+                        curr?.let { it1 -> comments.add(it1) }
+                    }
+
+                    threadCommentRVAdapter.notifyDataSetChanged()
+                }
+            }
+    }
+
+    override fun onItemClick(position: Int) {
+        TODO("Not yet implemented")
     }
 }
