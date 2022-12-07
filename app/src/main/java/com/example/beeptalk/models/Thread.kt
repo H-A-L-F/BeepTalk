fb38d8f6a5fc30a1d788a5941867aa8a32acee49
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-07 16:23:14 +0700

    Update Thread.kt

--- app/src/main/java/com/example/beeptalk/models/Thread.kt
+++ app/src/main/java/com/example/beeptalk/models/Thread.kt
@@ -2,18 +2,19 @@ package com.example.beeptalk.models
 
 import com.google.firebase.Timestamp
 import java.util.*
+import kotlin.collections.ArrayList
 
 data class Thread(
     var id: String? = null,
     val body : String? = null,
     val stitch : String? = null,
-    var upvote : Int = 0,
-    var downvote : Int = 0,
+    var upvote : ArrayList<String> = arrayListOf<String>(),
+    var downvote : ArrayList<String> = arrayListOf<String>(),
     val createdAt : Date = Timestamp.now().toDate(),
     var upDownFlag : Int = 0
 ) {
 
     public fun getTotalVotes(): Int {
-        return upvote - downvote
+        return upvote.size - downvote.size
     }
 }
