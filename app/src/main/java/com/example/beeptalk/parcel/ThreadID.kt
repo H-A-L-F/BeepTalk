0c40950d2656d9a4360f572a1f7113846d99a1b0
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-07 16:34:34 +0700

    fix thread page and thread parcel

--- app/src/main/java/com/example/beeptalk/parcel/ThreadID.kt
+++ app/src/main/java/com/example/beeptalk/parcel/ThreadID.kt
@@ -4,13 +4,14 @@ import android.os.Parcelable
 import com.google.firebase.Timestamp
 import java.util.*
 import kotlinx.parcelize.Parcelize
+import kotlin.collections.ArrayList
 
 @Parcelize
 class ThreadID(
     var id: String,
     val body : String,
     val stitch : String?,
-    var upvote : Int,
-    var downvote : Int,
+    var upvote : ArrayList<String>,
+    var downvote : ArrayList<String>,
     val createdAt : Date
 ): Parcelable
