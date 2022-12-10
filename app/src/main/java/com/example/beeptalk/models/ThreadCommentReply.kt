252d2588e81c3b25c9b6b3f7f56b37140b47f596
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-10 13:22:17 +0700

    add uid to commRep and post commRep

--- app/src/main/java/com/example/beeptalk/models/ThreadCommentReply.kt
+++ app/src/main/java/com/example/beeptalk/models/ThreadCommentReply.kt
@@ -4,6 +4,7 @@ data class ThreadCommentReply(
     var id: String? = null,
     val threadId: String? = null,
     val commentId: String? = null,
+    val uid: String? = null,
     val body : String? = null,
     val replyTo: String = "Default",
     var upvote : ArrayList<String> = arrayListOf<String>(),
--- app/src/main/java/com/example/beeptalk/pages/CommentDetailPage.kt
+++ app/src/main/java/com/example/beeptalk/pages/CommentDetailPage.kt
@@ -12,6 +12,7 @@ import com.example.beeptalk.lib.RecyclerViewInterface
 import com.example.beeptalk.lib.ThreadCommentRVAdapter
 import com.example.beeptalk.models.Notification
 import com.example.beeptalk.models.ThreadComment
+import com.example.beeptalk.models.ThreadCommentReply
 import com.example.beeptalk.parcel.ThreadCommentID
 import com.example.beeptalk.parcel.ThreadID
 import com.google.firebase.firestore.FirebaseFirestore
@@ -25,12 +26,55 @@ class CommentDetailPage : AppCompatActivity(), RecyclerViewInterface {
     private lateinit var comments : ArrayList<ThreadComment>
     private lateinit var threadCommentRVAdapter: ThreadCommentRVAdapter
 
+    private lateinit var currThread: ThreadID
+    private lateinit var currComm: ThreadCommentID
+
+    private var uid: String = "default"
+    private var uname: String = "default"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCommentDetailPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       
+        currThread = intent.getParcelableExtra("thread")!!
+        currComm = intent.getParcelableExtra("comment")!!
+
+       binding.apply {
+            replyToTv.text = currComm.replyTo
+            commentBodyTv.text = currComm.body
+            totalVotesTv.text = (currComm.upvote.size - currComm.downvote.size).toString()
+       }
+
+        db = FirebaseFirestore.getInstance()
+
+        sp = getSharedPreferences("current_user", Context.MODE_PRIVATE)
+        uid = sp.getString("uid", "default")!!
+        uname = sp.getString("username", "default")!!
+
+        binding.btnPostComment.setOnClickListener {
+            val body = binding.commentBodyEt.text.toString()
+            val threadId = currThread.id
+            val commId = currComm.id
+
+            val threadCommentReply = ThreadCommentReply(threadId = threadId, commentId = commId, body = body, uid = uid)
+
+            db.collection("threads").document(threadId)
+                .collection("comments").document(commId)
+                .collection("comments")
+                .add(threadCommentReply).addOnSuccessListener {
+                    binding.commentBodyEt.text.clear()
+
+                    Toast.makeText(this, "Posted comment", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    Toast.makeText(this, "Comment failed", Toast.LENGTH_SHORT).show()
+                }
+
+            val notification = Notification(uid, uname, "Replied on your comment")
+
+            db.collection("notifications").document(uid!!)
+                .collection("activities").add(notification)
+        }
     }
 
     private fun getThreadComments(threadId : String, commentId: String) {
