bcc17b4758f127ded75ef74fe0b311868796470e
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-10 11:02:26 +0700

    threadcommentReplyrvadapter

---
+++ app/src/main/java/com/example/beeptalk/lib/ThreadCommentReplyRVadapter.kt
@@ -0,0 +1,66 @@
+package com.example.beeptalk.lib
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.beeptalk.databinding.CardCommentThreadBinding
+import com.example.beeptalk.models.ThreadComment
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ThreadCommentReplyRVadapter(
+    private val comments: ArrayList<ThreadComment>,
+    private val recyclerViewInterface : RecyclerViewInterface,
+    private val uname: String,
+    private val uid: String
+): RecyclerView.Adapter<ThreadCommentReplyRVadapter.ViewHolder>() {
+
+    class ViewHolder(val binding: CardCommentThreadBinding, val recyclerViewInterface: RecyclerViewInterface): RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                if(bindingAdapterPosition != RecyclerView.NO_POSITION) {
+                    recyclerViewInterface.onItemClick(bindingAdapterPosition)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ThreadCommentReplyRVadapter.ViewHolder(
+            CardCommentThreadBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            ), recyclerViewInterface
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val comment = comments[position]
+        holder.binding.apply {
+            tvReply.text = comment.replyTo
+            tvTotalVotes.text = comment.getTotalVotes().toString()
+            tvCommentBody.text = comment.body
+        }
+
+        holder.binding.btnUpvote.setOnClickListener {
+            if(comment.upvote.contains(uid)) return@setOnClickListener
+            var db = FirebaseFirestore.getInstance()
+            db.collection("threads").document(comment.threadId!!)
+                .collection("comments").document(comment.id!!).update("upvote", FieldValue.increment(1))
+            holder.binding.tvTotalVotes.text = comment.getTotalVotes().toString()
+        }
+
+        holder.binding.btnDownvote.setOnClickListener {
+            if(comment.downvote.contains(uid)) return@setOnClickListener
+            var db = FirebaseFirestore.getInstance()
+            db.collection("threads").document(comment.threadId!!)
+                .collection("comments").document(comment.id!!).update("downvote", FieldValue.increment(1))
+            holder.binding.tvTotalVotes.text = comment.getTotalVotes().toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return comments.size
+    }
+}
