a2386b64f80eb32624c92ac6d27205d0a3f85af8
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-10 23:33:42 +0700

    notif set editText text

--- app/src/main/java/com/example/beeptalk/pages/EditThreadPage.kt
+++ app/src/main/java/com/example/beeptalk/pages/EditThreadPage.kt
@@ -3,16 +3,23 @@ package com.example.beeptalk.pages
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.beeptalk.databinding.ActivityEditThreadPageBinding
+import com.example.beeptalk.parcel.ThreadID
 
 class EditThreadPage : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditThreadPageBinding
+
+    private lateinit var currThread: ThreadID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditThreadPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        currThread = intent.getParcelableExtra("thread")!!
 
+        binding.apply {
+            etThreadBody.setText(currThread.body)
+        }
     }
 }
