f6cdc15f715a898ef3f166a6aafdfcf823f96dcf
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-11 10:00:42 +0700

    success edit and delete then redirect to mainPage

--- app/src/main/java/com/example/beeptalk/pages/EditThreadPage.kt
+++ app/src/main/java/com/example/beeptalk/pages/EditThreadPage.kt
@@ -1,13 +1,17 @@
 package com.example.beeptalk.pages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.beeptalk.databinding.ActivityEditThreadPageBinding
 import com.example.beeptalk.parcel.ThreadID
+import com.google.firebase.firestore.FirebaseFirestore
 
 class EditThreadPage : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditThreadPageBinding
+    private lateinit var db: FirebaseFirestore
 
     private lateinit var currThread: ThreadID
 
@@ -20,6 +24,30 @@ class EditThreadPage : AppCompatActivity() {
 
         binding.apply {
             etThreadBody.setText(currThread.body)
+        }
+
+        db = FirebaseFirestore.getInstance()
+
+        binding.btnPost.setOnClickListener {
+            val body = binding.etThreadBody.text.toString()
+
+            db.collection("threads").document(currThread.id)
+                .update("body", body).addOnSuccessListener {
+                    binding.etThreadBody.text.clear()
+
+                    Toast.makeText(this, "Edited thread", Toast.LENGTH_SHORT).show()
+                    intent = Intent(this, MainPage::class.java)
+                    startActivity(intent)
+                }
+        }
+
+        binding.btnDelete.setOnClickListener {
+            db.collection("threads").document(currThread.id)
+                .delete().addOnSuccessListener {
+                    Toast.makeText(this, "Deleted thread", Toast.LENGTH_SHORT).show()
+                    intent = Intent(this, MainPage::class.java)
+                    startActivity(intent)
+                }
         }
     }
 }
