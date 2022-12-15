621ec3b57f9e8c57ebb5ddae4362183f8c8c475e
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-16 02:23:58 +0700

    add Fragment binding

--- app/src/main/java/com/example/beeptalk/fragments/AddFragment.kt
+++ app/src/main/java/com/example/beeptalk/fragments/AddFragment.kt
@@ -7,17 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.beeptalk.R
+import com.example.beeptalk.databinding.FragmentAddBinding
 import com.example.beeptalk.pages.LoginPage
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class AddFragment : Fragment() {
 
+    private lateinit var binding : FragmentAddBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(layoutInflater, container, false)
+        
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
