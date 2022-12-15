4c46ffc05e5dca9858d7b1fb5cae29b96e8f0f76
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-12-16 04:27:03 +0700

    choose video in fragment, main, and fix id in fragment xml

--- app/src/main/java/com/example/beeptalk/fragments/AddFragment.kt
+++ app/src/main/java/com/example/beeptalk/fragments/AddFragment.kt
@@ -1,11 +1,21 @@
 package com.example.beeptalk.fragments
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.beeptalk.R
 import com.example.beeptalk.databinding.FragmentAddBinding
 import com.example.beeptalk.pages.LoginPage
@@ -15,12 +25,24 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 class AddFragment : Fragment() {
 
     private lateinit var binding : FragmentAddBinding
+    private lateinit var videoView : VideoView
+
+    // PICK VIDEO
+    private val VIDEO_PICK_GALLERY_CODE = 100
+    private val VIDEO_PICK_CAMERA_CODE = 101
+    private val CAMERA_REQUEST_CODE = 102
+    private val REQUEST_CODE = 1
+
+    private lateinit var cameraPermissions: Array<String>
+    private var videoUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAddBinding.inflate(layoutInflater, container, false)
+
+        videoView = binding.videoV
 
         return binding.root
     }
@@ -29,6 +51,105 @@ class AddFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
+    }
+
+    private fun setVideoToView() {
+        val mediaController = MediaController(context)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(videoUri)
+        videoView.requestFocus()
+        videoView.setOnPreparedListener {
+            videoView.pause()
+        }
+    }
+
+    private fun videoPickDialog() {
+        val options = arrayOf("Camera", "Gallery")
+
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Pick video from")
+            .setItems(options) {dialogInterface, i ->
+                if(i == 0) {
+                    if(!checkCameraPermissions()) requestCameraPermissions()
+                    else videoPickCamera()
+                } else {
+                    videoPickGallery()
+                }
+            }
+            .show()
+    }
+
+    private fun requestCameraPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            cameraPermissions,
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    private fun checkCameraPermissions(): Boolean {
+        val result1 = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val result2 = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+        return result1 && result2
+    }
+
+    private fun videoPickGallery() {
+        val intent = Intent()
+        intent.type = "video/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        startActivityForResult(
+            Intent.createChooser(intent, "Choose video"),
+            VIDEO_PICK_GALLERY_CODE
+        )
+    }
+
+    private fun videoPickCamera() {
+        val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+        startActivityForResult(intent, VIDEO_PICK_CAMERA_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if(grantResults.size > 0) {
+                    val cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    val storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    if(cameraAccepted && storageAccepted) videoPickCamera()
+                } else {
+                    Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == AppCompatActivity.RESULT_OK) {
+            if(requestCode == VIDEO_PICK_CAMERA_CODE) {
+                videoUri == data!!.data
+                setVideoToView()
+            } else if(requestCode == VIDEO_PICK_GALLERY_CODE) {
+                videoUri == data!!.data
+                setVideoToView()
+            }
+        } else {
+            Toast.makeText(context, "Cancelled", Toast.LENGTH_SHORT).show()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 }
